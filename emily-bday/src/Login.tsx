54270f4ba9241db1cd34a7f5de29d72e68ccd4ca
jlam999 <42471611+jlam999@users.noreply.github.com>
2023-06-19 18:22:43 -0500

    Finished login modal hopefully

--- emily-bday/src/Login.tsx
+++ emily-bday/src/Login.tsx
@@ -1,22 +1,67 @@
-import React from "react";
 import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
 
 const LoginModal = (): JSX.Element => {
-  return (
-    <div
-      className="modal show"
-      style={{ color: "black", display: "block", position: "initial" }}
-    >
-      <Modal.Dialog>
-        <Modal.Header closeButton>
-          <Modal.Title>Login</Modal.Title>
-        </Modal.Header>
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    password: "",
+  });
 
-        <Modal.Body>
-          <p>Modal body text goes here.</p>
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (formData.name == "Emily Zhao" && formData.password == "07032003") {
+      setLoggedIn(true);
+    } else {
+      alert("Wrong Name/Password");
+    }
+  };
+
+  return (
+    <Modal show={!isLoggedIn} centered size="lg">
+      <Modal.Header>
+        <Modal.Title className="mx-auto">Login</Modal.Title>
+      </Modal.Header>
+
+      <Form onSubmit={handleSubmit}>
+        <Modal.Body className="d-flex flex-column">
+          <Form.Group>
+            <Form.Label>Name</Form.Label>
+            <Form.Text muted> (First and Last Name) </Form.Text>
+            <Form.Control
+              onChange={(e) => {
+                setFormData({
+                  ...formData,
+                  name: e.target.value,
+                });
+              }}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Text> (Your Birthday MMDDYYYY)</Form.Text>
+            <Form.Control
+              type="password"
+              onChange={(e) => {
+                setFormData({
+                  ...formData,
+                  password: e.target.value,
+                });
+              }}
+            />
+          </Form.Group>
+          <Button
+            variant="primary"
+            type="submit"
+            className="mt-3 w-25 align-self-center"
+          >
+            Submit
+          </Button>
         </Modal.Body>
-      </Modal.Dialog>
-    </div>
+      </Form>
+    </Modal>
   );
 };
 
