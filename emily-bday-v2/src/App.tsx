65fcc6b483456657191de25308da4643cab2306e
jlam999 <42471611+jlam999@users.noreply.github.com>
2023-06-21 11:38:46 -0500

    made image background

--- emily-bday-v2/src/App.tsx
+++ emily-bday-v2/src/App.tsx
@@ -1,19 +1,26 @@
-import {
-  ChakraProvider,
-  Box,
-  theme,
-  Image,
-  Heading,
-} from "@chakra-ui/react";
+import { ChakraProvider, Box, theme, Heading } from "@chakra-ui/react";
 import LoginModal from "./LoginModal";
 import beach from "./beach_copy.jpeg";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <LoginModal />
-    <Box textAlign="center">
-      <Image src={beach} opacity={0.8} position="absolute" zIndex={-1}></Image>
-      <Heading>Happy Birthday Emily!</Heading>
+    <Box
+      textAlign="center"
+      backgroundImage={`url(${beach})`}
+      backgroundSize="cover"
+      aspectRatio={5 / 3}
+      w="100vw"
+      filter="opacity(75%)"
+    >
+      <Heading
+        fontSize="8xl"
+        position="relative"
+        top="32"
+        textShadow={"2px 2px 4px gray"}
+      >
+        Happy Birthday Emily!
+      </Heading>
     </Box>
   </ChakraProvider>
 );
