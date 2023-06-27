edbb542efd4c14e1e4b8c44280d13671181dba92
jlam999 <42471611+jlam999@users.noreply.github.com>
2023-06-27 16:24:37 -0500

    ft pics

--- emily-bday-v2/src/components/Section.tsx
+++ emily-bday-v2/src/components/Section.tsx
@@ -4,12 +4,12 @@ import { useInViewport } from "react-in-viewport";
 
 const Section = ({children} : {children: ReactNode}):JSX.Element => {
     const myRef = useRef(null);
-    const {inViewport} = useInViewport(myRef, {rootMargin: "-100px"}, {disconnectOnLeave: false});
+    const {inViewport, enterCount} = useInViewport(myRef, {rootMargin: "-100px"}, {disconnectOnLeave: false});
 
     return (
-      <SlideFade in={inViewport} offsetY="50px">
+      <SlideFade in={enterCount > 0} offsetY="50px">
         <Box ref={myRef} py={8} px={16}>
-          <Flex bgColor="pink.50" justify="space-evenly" align="center" p={4}>
+          <Flex bgColor="pink.50" justify="space-evenly" align="center" p={8}>
             {children}
           </Flex>
         </Box>
